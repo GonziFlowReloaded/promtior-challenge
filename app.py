5a85eae10f618d9083e7517fc49952f2e415c9b9
GonziflowReloaded <gonzalojoses@hotmail.com>
2025-03-20 00:49:46 -0300

    ADDED: FrontEnd to chat with the bot

--- app.py
+++ app.py
@@ -4,8 +4,13 @@ from core.config import settings
 from services.vector_service import initialize_vectorstore
 from services.chatbot_service import initialize_chatbot
 import uvicorn
+from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
 
 app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+
+# Templates
+templates = Jinja2Templates(directory="templates")
 
 # Include routers
 app.include_router(chat.router)
@@ -17,8 +22,13 @@ async def startup_event():
     initialize_chatbot()
 
 @app.get("/")
-async def root():
-    return {"message": f"{settings.PROJECT_NAME} API is running"}
+async def index(request: Request):
+    """Render of chatbot interface"""
+    
+    return templates.TemplateResponse("chat.html", 
+                                      {"request": request,
+                                       "title": settings.PROJECT_NAME,
+                                       })
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=settings.PORT, reload=settings.DEBUG)
